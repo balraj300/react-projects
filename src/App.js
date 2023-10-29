acc96bb7cd72a1ccd5f03c40628f02c17458f35a
balraj <balraj@balrajs-MacBook-Pro.local>
2023-10-29 08:00:55 +0530

    function of gameborad

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ function App() {
     <ol id="players">
 <Player name={"Player 1"} symbol={"X"}/>
 <Player name={"Player 2"} symbol={"0"}/>
-<Gameboard/>
 
     </ol>
-    GAME BORAD
+    <Gameboard/>
   </div>
   LOG
 </main>
--- src/Components/Gameboard.js
+++ src/Components/Gameboard.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
 const Gameboard = () => {
-  return (
-    <div>Gameboard</div>
-  )
-}
+    const initialGameBorad = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+        
 
+];
+  return (
+
+    <ol id='game-board'>
+        {initialGameBorad.map((row, rowIndex) => ( <li key = {rowIndex} >
+        <ol>
+            {row.map((playerSymbol,colIndex)=>(<li key={colIndex}>
+                <button>{playerSymbol}</button>
+                </li>
+            ))}
+            </ol>
+            </li>
+        ))}
+        </ol>
+  );
+
+  }
 export default Gameboard
